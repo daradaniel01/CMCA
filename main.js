a08bcc176825ce4e830cc7dff95f405429f27b18
daradaniel01 <daradanie.07@gmail.com>
2022-10-18 14:44:16 +0100

    jokjloij

---
+++ main.js
@@ -0,0 +1,53 @@
+var close = document.querySelector('.fa-eye')
+var open = document.querySelector('.fa-eye-slash')
+open.addEventListener("click", () => {
+    close.style.display='inline'
+    document.getElementById('password').setAttribute("type", 'text');
+    open.style.display='none'
+})
+close.addEventListener("click", () => {
+    open.style.display='inline'
+    document.getElementById('password').setAttribute("type", 'password');
+    close.style.display='none'
+})
+var x = 1
+var y = 2
+
+var sun = ""
+
+var _id = 1
+
+var $id = 100
+
+var day1 = 8
+
+var firstName = 'cent'
+
+var createIfNotExist;
+var createifnotexist;
+
+var firstname = 'cent'
+var first_Name = "cent" // slkdfja;sdlkfajsdf;asdlkfjasd;flkasdfasd;flkadsf
+// askljsdkj
+
+var message = "Bro, sorry I coudn't make it to the practice in time"
+
+// Boolean
+
+var isAdmin = true
+
+
+var day = ['javaScript', 'html', 'css', 2, '6']
+
+var add = Math.floor(Math.sqrt(126))
+
+console.log(add);
+
+if(day.length>0){
+  console.log(day)
+}else{
+  console.log('day is 0')
+}
+
+
+
---
+++ script.js
@@ -0,0 +1,26 @@
+// Declare the current date and future date
+const countdown = () => {
+    const newYear = new Date('october 7 2022');
+    const currentYear = new Date();
+    const timer = newYear - currentYear;
+  
+    //Assign each element a value
+    const seconds = 1000;
+    const minutes = seconds * 60;
+    const hours = minutes * 60;
+    const days = hours * 24;
+  
+    //Calculate each element declare the value
+    const cdDay = Math.floor(timer / days);
+    const cdHour = Math.floor((timer % days) / hours);
+    const cdMinute = Math.floor((timer % hours) / minutes);
+    const cdSecond = Math.floor((timer % minutes) / seconds);
+  
+    // Get all the required element by ID
+    document.getElementById('day').innerText = cdDay;
+    document.getElementById('hour').innerText = cdHour;
+    document.getElementById('minute').innerText = cdMinute;
+    document.getElementById('second').innerText = cdSecond;
+  };
+  setInterval(countdown, 1000);
+ 
